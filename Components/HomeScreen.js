1516127f1ab1dd4b2d5eae2a02af719c8ddf804d
MatyAntunez <matias.antunez410@gmail.com>
2022-06-24 18:27:52 -0300

    searchBar

--- Components/HomeScreen.js
+++ Components/HomeScreen.js
@@ -4,11 +4,13 @@ import { ActivityIndicator } from 'react-native';
 import apiParams from '../config.js';
 import axios from 'axios';
 import CharacterCard from '../Components/CharacterCard';
+import { Searchbar } from 'react-native-paper';
 
 export default function Home() {
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
   const { ts, apikey, hash, baseURL } = apiParams;
+  const  [search, setSearch] = useState('');
 
   useEffect(() => {
     axios.get(`${baseURL}/v1/public/characters`, {
@@ -23,22 +25,47 @@ export default function Home() {
       .finally(() => setLoading(false));
   }, []);
 
+  function searchCharacter() {
+    if(search) {
+      setLoading(true);
+      axios.get(`${baseURL}/v1/public/characters`, {
+        params: {
+          ts,
+          apikey,
+          hash,
+          nameStartsWith: search
+        }
+      })
+        .then(response => setData(response.data.data.results))
+        .catch(error => console.error(error))
+        .finally(() => setLoading(false));
+    }
+  }
+
   return (
-    <View>
+    <View style={{flex: 1}}>
       {isLoading 
         ? <ActivityIndicator size="large" color="#00ff00" /> 
         : (
-          <FlatList
-            data={data}
-            keyExtractor={({ id }) => id.toString()}
-            renderItem={({ item }) => (
-              <CharacterCard 
-              style={styles.image}
-                id={item.id}
-                image={`${item?.thumbnail?.path}.${item?.thumbnail.extension}`} 
-                name={item.name} />
-            )}
-          />
+          <>
+            <Searchbar
+           placeholder="Search for character..."
+           onChangeText={value => setSearch(value)}
+           value={search}
+           onIconPress={searchCharacter}
+           onSubmitEditing={searchCharacter}
+            />
+            <FlatList
+              data={data}
+              keyExtractor={({ id }) => id.toString()}
+              renderItem={({ item }) => (
+                <CharacterCard 
+                  id={item.id}
+                  image={`${item?.thumbnail?.path}.${item?.thumbnail.extension}`} 
+                  name={item.name} />
+              )}
+            />
+          </>
         )
       }
     </View>
